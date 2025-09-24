ab8d7c297b60400fba978bf163e1fa30876c6b90
big-basket <jadyn-hand@hotmail.co.uk>
2025-09-24 15:38:39 +0100

    okay back to npm

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+
+// Light
+const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+scene.add(light);
+
+// Object
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+cube.position.set(0, 0, -0.5); // half a meter in front
+scene.add(cube);
+
+// Animate
+function animate() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+// WebXR Button
+document.body.appendChild(THREE.XRButton.createButton(renderer, { requiredFeatures: ['hit-test'] }));
+
+renderer.setAnimationLoop(animate);
